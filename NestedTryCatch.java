7262e7b6436d4fe59c48067db2953923ab2e110c
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-04-04 09:23:47 +0530

    Problem to use nested try catch block using array operations to get exceptions

--- NestedTryCatch.java
+++ NestedTryCatch.java
@@ -4,34 +4,56 @@ public class NestedTryCatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        try {
-            System.out.print("Enter the size of the array: ");
-            int size = scanner.nextInt();
-            int[] array = new int[size];
-
-            System.out.println("Enter " + size + " elements:");
-            for (int i = 0; i < size; i++) {
-                array[i] = scanner.nextInt();
+        try{
+            int size = 4;
+            int[] arr = new int[size];
+            int divisor = 0;
+            try{
+                System.out.println(arr[5]);
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Invalid array index");
+                System.out.println(e.getMessage());
             }
-
-            System.out.print("Enter index to access: ");
-            int index = scanner.nextInt();
-
-            try {
-                System.out.print("Enter divisor: ");
-                int divisor = scanner.nextInt();
-
-                int result = array[index] / divisor;
-                System.out.println("Result: " + result);
-            } catch (ArithmeticException e) {
-                System.out.println("Cannot divide by zero!");
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Invalid array index!");
-        } catch (Exception e) {
-            System.out.println("Invalid input. Please enter valid numbers.");
-        } finally {
-            scanner.close();
+//            try{
+                System.out.println(arr[3]/divisor);
+//            }
+        } catch (ArithmeticException e){
+            System.out.println("Can not divide by zero");
+            System.out.println(e.getMessage());
         }
+        catch (Exception e){
+            System.out.println("Some error occured");
+            System.out.println(e.getMessage());
+        }
+
+//        try {
+//            System.out.print("Enter the size of the array: ");
+//            int size = scanner.nextInt();
+//            int[] array = new int[size];
+//
+//            System.out.println("Enter " + size + " elements:");
+//            for (int i = 0; i < size; i++) {
+//                array[i] = scanner.nextInt();
+//            }
+//
+//            System.out.print("Enter index to access: ");
+//            int index = scanner.nextInt();
+//
+//            try {
+//                System.out.print("Enter divisor: ");
+//                int divisor = scanner.nextInt();
+//
+//                int result = array[index] / divisor;
+//                System.out.println("Result: " + result);
+//            } catch (ArithmeticException e) {
+//                System.out.println("Cannot divide by zero!");
+//            }
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("Invalid array index!");
+//        } catch (Exception e) {
+//            System.out.println("Invalid input. Please enter valid numbers.");
+//        } finally {
+//            scanner.close();
+//        }
     }
 }
